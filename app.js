09b0ea0e8bd4c9dea657e2b09dd6bdac44bc7193
vtn-hz <hernandezpascualvalentino@gmail.com>
2024-01-29 23:37:22 -0300

    dev,add custom command

--- app.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('discord.js');
-const { token  } = require('./config.json');
-
-console.log(token);
-const client = new Client({ intents: [] });
-client.login(token);
-
---
+++ src/app.js
@@ -0,0 +1,75 @@
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { token, clientId, guildId  } = require('./etc/config.json');
+const { REST, Routes } = require('discord.js');
+
+const ping  = require ('./commands/util/ping.js')
+
+const rest = new REST().setToken(token);
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ]
+});
+
+
+client.commands = new Collection ();
+client.commands.set(ping.data.name, ping);
+
+
+const commands = [];
+commands.push(ping.data.toJSON());
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+});
+
+
+client.on('messageCreate', (message) => {
+    console.log(`Mensaje recibido: ${message.content}`);
+});
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		//console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.login(token);
+
+// Log in to Discord with your client's token
+client.login(token);
